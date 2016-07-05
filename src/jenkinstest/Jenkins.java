0160addab2fd74c314703854afd56c10a5889a3c
rucha-trivedi <rucha.trivedi@workhiveapp.com>
2016-07-05 06:50:44 +0000

    first file

---
+++ src/jenkinstest/Jenkins.java
@@ -0,0 +1,14 @@
+package jenkinstest;
+
+class test{
+	test()
+	{
+		System.out.println("Test Constructor");
+	}
+}
+public class Jenkins {
+	public static void main(String[] args) {
+		test t = new test();
+		System.out.println("Done");
+	}
+}
