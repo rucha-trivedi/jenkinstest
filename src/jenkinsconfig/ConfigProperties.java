d7874db5da3de9a5c2c7b3b1625747a2ce614d70
rucha-trivedi <rucha.trivedi@workhiveapp.com>
2016-07-06 05:40:31 +0000

    new message for second folder

--- src/jenkinsconfig/ConfigProperties.java
+++ src/jenkinsconfig/ConfigProperties.java
@@ -3,6 +3,7 @@ package jenkinsconfig;
 public class ConfigProperties {
 	public static void main(String[] args) {
 		System.out.println("Congig file package");
+		System.out.println("Config properties demo");
 	}
 
 }
