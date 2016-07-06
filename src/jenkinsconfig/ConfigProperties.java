18bd6269dc747202b35bd3fdd31a6d6cf587f923
rucha-trivedi <rucha.trivedi@workhiveapp.com>
2016-07-06 05:45:29 +0000

    couldnot fetch to jenkins being offline

--- src/jenkinsconfig/ConfigProperties.java
+++ src/jenkinsconfig/ConfigProperties.java
@@ -4,6 +4,7 @@ public class ConfigProperties {
 	public static void main(String[] args) {
 		System.out.println("Congig file package");
 		System.out.println("Config properties demo");
+		System.out.println("Server offline test successful for push");
 	}
 
 }
